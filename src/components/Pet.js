3dce6f316158d3be27673ac07ec3ba22cf3595be
bryanjug <bryandjug@gmail.com>
2021-03-28 22:14:22 -0700

    added styled components

---
+++ src/components/Pet.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Pet extends React.Component {
+    render() {
+        return (
+            <div>
+                <img src="../../img/white_cat.png" className="w-25 mx-auto d-block" alt=""/>
+            </div>
+        );
+    }
+}
+
+export default Pet;
---
+++ src/components/Time.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Time extends React.Component {    
+    render() {
+        return (
+            <div>
+                <p style={{fontSize: '550%'}} className="text-light text-center">25:00</p>
+            </div>
+        );
+    }
+};
+
+export default Time;
