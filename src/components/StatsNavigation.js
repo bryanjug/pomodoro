1d32c67f8454831fa699c55e90db90cde72e1efb
bryanjug <bryandjug@gmail.com>
2021-04-21 03:26:47 -0700

    added pet size at mounted, breaks other useEffect

--- src/components/StatsNavigation.js
+++ src/components/StatsNavigation.js
@@ -1,22 +1,3 @@
-import {Link} from 'react-router-dom';
-
-const StatsNavigation = () => {
-    return (
-        <div className="text-center pt-4 pb-4">
-            <Link to="/stats/day">
-                <button type="button" className="btn btn-secondary statsNavigation statsNavigationLeft">Day</button>
-            </Link>
-            <Link to="/stats/week">
-                <button type="button" className="btn btn-secondary statsNavigation">Week</button>
-            </Link>
-            <Link to="/stats/month">
-                <button type="button" className="btn btn-secondary statsNavigation">Month</button>
-            </Link>
-            <Link to="/stats/year">
-                <button type="button" className="btn btn-secondary statsNavigation statsNavigationRight">Year</button>
-            </Link>
-        </div>
-    );
-}
-
-export default StatsNavigation;
+version https://git-lfs.github.com/spec/v1
+oid sha256:2d09e7569916e2637d957fe3ef2d9fa4bc7af4146f0c4e8ebf37977e018d33d8
+size 846
--- src/index.js
+++ src/index.js
@@ -1,21 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import App from "./components/App";
-import Timer from './components/Timer';
-import DayStats from './components/DayStats';
-import WeekStats from './components/WeekStats';
-import MonthStats from './components/MonthStats';
-import YearStats from './components/YearStats';
-
-ReactDOM.render(
-    <Router>
-        <Route exact path="/" component={Timer} />
-        <Route path="/stats/day" component={DayStats} />
-        <Route path="/stats/week" component={WeekStats} />
-        <Route path="/stats/month" component={MonthStats} />
-        <Route path="/stats/year" component={YearStats} />
-        <App />
-    </Router>, 
-    document.querySelector("#root")
-);
+version https://git-lfs.github.com/spec/v1
+oid sha256:18dbd20a82911d68d26b7a2cf1ebbb9e08d80cad3428868e167358c57963f4f3
+size 783
